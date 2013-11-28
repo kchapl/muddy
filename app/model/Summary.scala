9dac96c86fbd1b7bce0de65d5f2aa245100d1dbb
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-11-28 09:43:43 +0000

    Previous months

--- app/model/Summary.scala
+++ app/model/Summary.scala
@@ -16,34 +16,67 @@ class Summary {
     start = startDate.minusMonths(1), end = endDate.minusMonths(1)
   ).toList.sortBy(_.date.getMillis)
 
+  private val prevYearTransactions: List[Transaction] = TransactionService.getTransactions(
+    start = startDate.minusYears(1), end = endDate.minusYears(1)
+  ).toList.sortBy(_.date.getMillis)
+
   val lastTransactionDate: Option[DateTime] = transactions.lastOption.map(_.date)
 
   val categorySummaries: List[CategorySummary] = {
     val prevCategories = prevTransactions.groupBy(_.category)
+    val prevYearCategories = prevYearTransactions.groupBy(_.category)
     transactions.groupBy(_.category).map {
       case (category, txs) => {
         val name = category getOrElse "uncategorised"
         val currAmt = round(txs.map(_.amount).sum)
         val prevTxs = prevCategories.get(category).getOrElse(Nil)
+        val prevYearTxs = prevYearCategories.get(category).getOrElse(Nil)
         val prevAmt = round(prevTxs.map(_.amount).sum)
-        CategorySummary(name, currAmt, prevAmt)
+        val prevYearAmt = round(prevYearTxs.map(_.amount).sum)
+        CategorySummary(name, currAmt, prevAmt, prevYearAmt)
       }
     }.toList
   }
 
-  private def sumFiltered(p: Double => Boolean) = round(transactions.map(_.amount).filter(p).sum)
+  private def sumFiltered(p: Double => Boolean)(txs: Seq[Transaction]) = round(txs.map(_.amount).filter(p).sum)
 
-  val paymentsTotalAmount = sumFiltered(_ < 0)
-  val depositsTotalAmount = sumFiltered(_ > 0)
-  val difference = sumFiltered(_ => true)
+  private def sumPayments = sumFiltered(_ < 0)(_)
+
+  private def sumDeposits = sumFiltered(_ > 0)(_)
+
+  private def sumAll = sumFiltered(_ => true)(_)
+
+  val paymentsTotalAmount = sumPayments(transactions)
+  val depositsTotalAmount = sumDeposits(transactions)
+  val difference = sumAll(transactions)
+
+  private val prevDifference = sumAll(prevTransactions)
+  val differencePreviousMonth = prevDifference - difference
+  val percentageDifferencePreviousMonth = {
+    if (prevDifference == 0) 999
+    else differencePreviousMonth / prevDifference
+  }
+
+  private val prevYearDifference = sumAll(prevYearTransactions)
+  val differencePreviousYear = prevYearDifference - difference
+  val percentageDifferencePreviousYear = {
+    if (prevYearDifference == 0) 999
+    else differencePreviousYear / prevYearDifference
+  }
 }
 
-case class CategorySummary(category: String, currentAmount: Double, previousAmount: Double) {
+case class CategorySummary(category: String, currentAmount: Double, previousAmount: Double, previousYearAmount: Double) {
 
   val difference = previousAmount - currentAmount
+  val yearDifference = previousYearAmount - currentAmount
 
   val percentageDifference = {
     if (previousAmount == 0) 999
     else difference / previousAmount
+  }
+
+  val percentageYearDifference = {
+    if (previousYearAmount == 0) 999
+    else difference / previousYearAmount
   }
 }
