fb1e48fa70e8eeb36161dea79984a01ce76d887b
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-12-13 11:58:00 +0000

    wip

--- app/model/Test.scala
+++ app/model/Test.scala
@@ -15,17 +15,34 @@ object Test {
     "Transfer to other account"
   )
 
-  def payments(startDate: DateTime, endDate: DateTime) = TransactionRepository.all().filter {
-    tx => !tx.date.isBefore(startDate) && tx.date.isBefore(endDate) && tx.amount < 0
+  def payments(from: DateTime, to: DateTime) = TransactionRepository.all().filter {
+    tx => !tx.date.isBefore(from) && tx.date.isBefore(to) && tx.amount < 0
   }.sortBy(_.date.getMillis)
 
-  def outgoings(startDate: DateTime, endDate: DateTime) = payments(startDate, endDate).filterNot {
+  def outgoings(from: DateTime, to: DateTime) = payments(from, to).filterNot {
     tx => transferCategories.contains(tx.category)
   }.sortBy(_.date.getMillis)
 
-  def sumPayments(startDate: DateTime, endDate: DateTime) = payments(startDate, endDate).map(_.amount).sum
+  def sumPayments(from: DateTime, to: DateTime) = payments(from, to).map(_.amount).sum
 
-  def sumOutgoings(startDate: DateTime, endDate: DateTime) = outgoings(startDate, endDate).map(_.amount).sum
+  def sumOutgoings(from: DateTime, to: DateTime) = outgoings(from, to).map(_.amount).sum
 
   def allCategories = TransactionRepository.all().map(_.category.getOrElse("none")).sorted.distinct
+
+  def period(from: DateTime, to: DateTime): Period = {
+    Period(
+      startDate = from,
+      endDate = to,
+      outgoings = sumOutgoings(from, to),
+      difference = Difference2(0, 0)
+    )
+  }
+
+  def difference(current:Double,reference:Double):Difference2 = {
+
+  }
 }
+
+case class Difference2(amount: Double, percentage: Double)
+
+case class Period(startDate: DateTime, endDate: DateTime, outgoings: Double, difference: Difference2)
